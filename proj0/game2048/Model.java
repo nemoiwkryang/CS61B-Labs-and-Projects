1ff070c97bc82f51365c88afe04a17f2db7cd74b
nemoiwkryang <yanggr2025@shanghaitech.edu.cn>
2026-04-01 09:50:22 +0800

    0401 Update

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -138,6 +138,13 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        for (int i=0;i<b.size();i++){
+            for (int j=0;j<b.size();j++){
+                if (b.tile(i,j) == null){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -148,6 +155,13 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        for (int i=0;i<b.size();i++){
+            for (int j=0;j<b.size();j++){
+                if (b.tile(i,j) != null && b.tile(i,j).value() == MAX_PIECE){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -159,6 +173,19 @@ public class Model extends Observable {
      */
     public static boolean atLeastOneMoveExists(Board b) {
         // TODO: Fill in this function.
+        if (emptySpaceExists(b)){
+            return true;
+        }
+        for (int i=0;i<b.size();i++){
+            for (int j=0;j<b.size();j++){
+                if(i<b.size()-1 && b.tile(i,j).value()==b.tile(i+1,j).value()){
+                    return true;
+                }
+                if(j<b.size()-1 && b.tile(i,j).value()==b.tile(i,j+1).value()){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
